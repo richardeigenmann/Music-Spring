8db393fadd30602f5820d4c6466c453eaa08368f
Richard Eigenmann <richard.eigenmann@gmail.com>
2026-02-16 18:36:40 +0100

    Add click handler to go to the home route

--- musicfrontend/src/app/app.ts
+++ musicfrontend/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('musicfrontend');
+
+  constructor(private router: Router) {}
+
+  goHome(): void {
+    this.router.navigate(['/playlists']);
+  }
 }
